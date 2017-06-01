e65259e3d6904a7edad46a78756c1683920ab640
EddyRonald <eddy.choque.condori@ucsp.edu.pe>
2017-06-01 17:57:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "Producto.h"
+#include "Laptop.h"
 
 using namespace std;
 
 int main()
 {
-	Producto p;
-	p.print();
-	
+	//Producto p;
+	//p.print();
+	Laptop l;
+	l.print();
+
 	return 0;
 }
