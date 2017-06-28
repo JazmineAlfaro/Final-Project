a1f219b2179c8247ece8c805d59de5e5c17e2c4b
EddyRonald <eddy.choque.condori@ucsp.edu.pe>
2017-06-28 00:14:25 -0500

    funciones implementadas a DB "Recuperar,Insertar,Salvar,Visuailizar,Seleccionar" solo falta eliminar , consultar y la clase menu.

---
+++ BDIIl/include/Crear.h
@@ -0,0 +1,17 @@
+#ifndef CREAR_H
+#define CREAR_H
+#include "Ask.h"
+#include "Producto.h"
+
+class Crear
+{
+    public:
+        Crear();
+        virtual ~Crear();
+
+    protected:
+
+    private:
+};
+
+#endif // CREAR_H
